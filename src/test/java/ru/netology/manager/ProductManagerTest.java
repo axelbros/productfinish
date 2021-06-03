3c241a6ef8bd6a50329abcb60aaf9e5c3d4a5bcf
Alexander Ivankov <ffmoneyff@gmail.com>
2021-06-03 15:03:44 +0300

    manager

---
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -0,0 +1,47 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.repository.ProductRepository;
+import ru.netology.ru.netology.manager.ProductManager;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ProductManagerTest {
+    @Test
+    void shouldSearchByAuthor() {
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager = new ProductManager(repository);
+        Product[] expected = {new Book(1, "Little Women", 100, "Louisa May Alcott")};
+        Product[] actual = manager.searchBy("Louisa May Alcott");
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldSearchByBookName() {
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager = new ProductManager(repository);
+        Product[] expected = {new Book(2, "The Adventures of Sherlock Holmes", 200, "A. Conan Doyle")};
+        Product[] actual = manager.searchBy("The Adventures of Sherlock Holmes");
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldSearchBySmartphoneName() {
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager = new ProductManager(repository);
+        Product[] expected = {new Smartphone(4, "iPhone XR", 1000, "Apple")};
+        Product[] actual = manager.searchBy("iPhone XR");
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldSearchBySmartphoneManufacturer() {
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager = new ProductManager(repository);
+        Product[] expected = {new Smartphone(3, "A53", 500, "Samsung")};
+        Product[] actual = manager.searchBy("Samsung");
+        assertArrayEquals(expected, actual);
+    }
+}
+
+
